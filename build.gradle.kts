84fee7c7ca4a542d2302fced46a7c8a61176f0dd
al-volkov <70965603+al-volkov@users.noreply.github.com>
2021-10-04 22:59:04 +0300

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.5.31"
+    id("io.gitlab.arturbosch.detekt").version("1.18.0")
 }
 
 group = "com.code_labeler"
@@ -23,4 +24,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+}
+
+detekt {
+    buildUponDefaultConfig = true
 }
