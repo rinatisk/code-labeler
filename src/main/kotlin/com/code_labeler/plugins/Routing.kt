9d0c0aa70f99d29579a8f0a1c771de33c31fba5c
al-volkov <70965603+al-volkov@users.noreply.github.com>
2021-10-08 22:11:11 +0300

    added file upload/download

--- src/main/kotlin/com/code_labeler/plugins/Routing.kt
+++ src/main/kotlin/com/code_labeler/plugins/Routing.kt
@@ -1,16 +1,60 @@
 package com.code_labeler.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.routing
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.PartData
+import io.ktor.http.content.streamProvider
+import io.ktor.http.HttpHeaders
+import io.ktor.http.ContentDisposition
+import io.ktor.response.respondText
+import io.ktor.response.header
+import io.ktor.response.respondFile
+import io.ktor.request.receiveMultipart
+import java.io.File
+import java.util.UUID
 
 fun Application.configureRouting() {
-    // Starting point for a Ktor app:
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        var fileDescription = ""
+        var fileName = ""
+        var uuid = ""
+        post("/upload") {
+            val multipartData = call.receiveMultipart()
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        fileDescription = part.value
+                    }
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName as String
+                        val fileBytes = part.streamProvider().readBytes()
+                        uuid = UUID.randomUUID().toString()
+                        File("files/$uuid").writeBytes(fileBytes)
+                    }
+                }
+            }
+
+            call.respondText("file uploaded successfully to files/$uuid")
+        }
+
+        get("/download/{id}") {
+            val id = call.parameters["id"]
+            val file = File("files/$id")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "$id.csv")
+                    .toString()
+            )
+            call.respondFile(file)
         }
     }
 
