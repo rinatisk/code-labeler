54a95f530b2908579ceca51bf92e1cba7da3f3cd
al-volkov <70965603+al-volkov@users.noreply.github.com>
2021-10-10 21:05:10 +0300

    db added

--- src/main/kotlin/com/code_labeler/Application.kt
+++ src/main/kotlin/com/code_labeler/Application.kt
@@ -6,6 +6,7 @@ import com.code_labeler.plugins.*
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
     embeddedServer(Tomcat, port = 8080, host = "0.0.0.0") {
@@ -13,5 +14,15 @@ fun main() {
             json()
         }
         configureRouting()
+        initDB()
     }.start(wait = true)
+}
+
+fun initDB() {
+    Database.connect(
+        "jdbc:postgresql://localhost:5432/postgres",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = ""
+    )
 }
