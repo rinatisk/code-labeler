6d73dd23a00e48378c213cdadfd9e0e99c108e98
al-volkov <70965603+al-volkov@users.noreply.github.com>
2021-10-10 17:18:53 +0300

    first working version

--- src/main/kotlin/com/code_labeler/Utils.kt
+++ src/main/kotlin/com/code_labeler/Utils.kt
@@ -2,13 +2,28 @@ package com.code_labeler
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import kotlinx.serialization.SerialName
 import java.io.File
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
+@Serializable
 open class Label(val name: String) {
     override fun toString() = name
 }
 
-class CodeWithLabel(val code: String, val label: Label) {
+@Serializable
+class CodeWithLabel(val code: String, var label: Label) {
+}
+
+@Serializable
+class NewLabel(val numberOfSnippet: Int, val label: Label) {
+}
+
+fun parseCsvString(string: String): List<CodeWithLabel> {
+    val map = csvReader().readAllWithHeader(string)
+    return map.map { CodeWithLabel(it.getOrDefault("code", ""), Label(name = it.getOrDefault("value", ""))) }
 }
 
 fun parseCsvFile(file: File): List<CodeWithLabel> {
@@ -22,4 +37,3 @@ fun marshalCsvFile(listOfSnippets: List<CodeWithLabel>, file: File) {
     val fullList = title + rows
     csvWriter().writeAll(fullList, file)
 }
-
--- src/main/kotlin/com/code_labeler/plugins/Routing.kt
+++ src/main/kotlin/com/code_labeler/plugins/Routing.kt
@@ -1,19 +1,20 @@
 package com.code_labeler.plugins
 
-import io.ktor.routing.routing
-import io.ktor.routing.get
-import io.ktor.routing.post
+import com.code_labeler.CodeWithLabel
+import com.code_labeler.NewLabel
+import com.code_labeler.marshalCsvFile
+import com.code_labeler.parseCsvString
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.http.*
 import io.ktor.http.content.forEachPart
 import io.ktor.http.content.PartData
 import io.ktor.http.content.streamProvider
-import io.ktor.http.HttpHeaders
-import io.ktor.http.ContentDisposition
-import io.ktor.response.respondText
-import io.ktor.response.header
-import io.ktor.response.respondFile
-import io.ktor.request.receiveMultipart
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import java.io.File
 import java.util.UUID
 
@@ -36,9 +37,10 @@ fun Application.configureRouting() {
                     }
                     is PartData.FileItem -> {
                         fileName = part.originalFileName as String
-                        val fileBytes = part.streamProvider().readBytes()
+                        val list = parseCsvString(String(part.streamProvider().readBytes()))
+                        val string = Json.encodeToString(list)
                         uuid = UUID.randomUUID().toString()
-                        File("files/$uuid").writeBytes(fileBytes)
+                        File("files/$uuid").writeText(string)
                     }
                 }
             }
@@ -49,12 +51,32 @@ fun Application.configureRouting() {
         get("/download/{id}") {
             val id = call.parameters["id"]
             val file = File("files/$id")
+            val temporaryFile = File("temporaryFiles/$id.csv")
+            val list: List<CodeWithLabel> = Json.decodeFromString(file.readText())
+            marshalCsvFile(list, temporaryFile)
             call.response.header(
                 HttpHeaders.ContentDisposition,
                 ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "$id.csv")
                     .toString()
             )
-            call.respondFile(file)
+            call.respondFile(temporaryFile)
+            temporaryFile.delete()
+        }
+
+        delete("files/{id}") {
+            val id = call.parameters["id"]
+            File("files/$id").delete()
+            call.respond(HttpStatusCode.OK, "OK")
+        }
+
+        put("files/{id}") {
+            val id = call.parameters["id"]
+            val newLabel = call.receive<NewLabel>()
+            val file = File("files/$id")
+            val list: List<CodeWithLabel> = Json.decodeFromString(file.readText())
+            list[newLabel.numberOfSnippet].label = newLabel.label
+            file.writeText(Json.encodeToString(list))
+            call.respond(HttpStatusCode.OK, "OK")
         }
     }
 
