6b7b5e654f6d4a6e9cc6d5a4d29f2f779492f32c
rinatisk <st087501@student.spbu.ru>
2021-10-11 14:43:42 +0300

    revert

--- src/test/kotlin/com/code_labeler/UtilsKtTest.kt
+++ src/test/kotlin/com/code_labeler/UtilsKtTest.kt
@@ -14,7 +14,7 @@ internal class UtilsKtTest {
         val list = listOf(CodeWithLabel("ada", Label("daad")), CodeWithLabel("d", Label("dd")))
         marshalCsvFile(list, tempResource)
         val expectedFile = this.javaClass.getResource("marshalTest/expectedFile1.csv")
-        assertEquals(expectedFile.readText(), tempResource.readText().replace("\r", ""));
+        assertEquals(expectedFile.readText().replace("\r", ""), tempResource.readText().replace("\r", ""));
     }
 
     @Test
@@ -26,7 +26,7 @@ internal class UtilsKtTest {
         )
         marshalCsvFile(list, tempResource)
         val expectedFile = this.javaClass.getResource("marshalTest/expectedFile2.csv")
-        assertEquals(expectedFile.readText(), tempResource.readText().replace("\r", ""))
+        assertEquals(expectedFile.readText().replace("\r", ""), tempResource.readText().replace("\r", ""))
 
     }
 
@@ -39,6 +39,6 @@ internal class UtilsKtTest {
 
         val expectedFile = this.javaClass.getResource("marshalTest/expectedFile3.csv")
 
-        assertEquals(expectedFile.readText(), tempResource.readText().replace("\r", ""))
+        assertEquals(expectedFile.readText().replace("\r", ""), tempResource.readText().replace("\r", ""))
     }
 }
