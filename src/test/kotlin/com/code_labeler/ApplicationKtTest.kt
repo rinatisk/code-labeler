4cb580d482aa289ec6bc2f881f56af12fe7df35e
rinatisk <st087501@student.spbu.ru>
2021-10-11 14:59:28 +0300

    fix file name

--- src/test/kotlin/com/code_labeler/ApplicationKtTest.kt
+++ src/test/kotlin/com/code_labeler/ApplicationKtTest.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 internal class ApplicationKtTest {
     @Test
     fun parse() {
-        val file = this.javaClass.getResource("simple_file.csv").file
+        val file = this.javaClass.getResource("simpleFile.csv").file
         val actual = parseCsvFile(File(file))
 
         assertEquals("example1 { }", actual.first().code)
